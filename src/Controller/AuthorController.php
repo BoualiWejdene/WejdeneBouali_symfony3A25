d12ccf7b5c5c5092c5247d9da13ed35e4367e292
BoualiWejdene <146763358+BoualiWejdene@users.noreply.github.com>
2025-09-27 10:27:00 +0100

    Atelier TWIG : exercice 2 terminé

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -15,11 +15,32 @@ final class AuthorController extends AbstractController
             'controller_name' => 'AuthorController',
         ]);
     }
+    
 
     #[Route('/show/{name}',name:'showAuthor')]
     public function showAuthor($name){
         return $this->render('author/show.html.twig',['nom' => $name, 'prenom' => 'ben foulen']);
     }
 
+
+    #[Route('/authors',name:'listAuthors')]
+    public function listAuthors(){
+        $authors = array(
+            array('id' => 1, 'picture' => 'assets/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100),
+            array('id' => 2, 'picture' => 'assets/images/william-shakespeare.jpg','username' => ' William Shakespeare', 'email' =>  ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
+            array('id' => 3, 'picture' => 'assets/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300),
+        );
+        return $this->render('author/list.html.twig',['auteurs' => $authors]);
+    }
+
+    #[Route('/authors/{id}',name:'authorDetails')]
+    public function authorDetails($id){
+        $authors = array(
+            array('id' => 1, 'picture' => 'assets/images/Victor-Hugo.jpg','username' => 'Victor Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100),
+            array('id' => 2, 'picture' => 'assets/images/william-shakespeare.jpg','username' => ' William Shakespeare', 'email' =>  ' william.shakespeare@gmail.com', 'nb_books' => 200 ),
+            array('id' => 3, 'picture' => 'assets/images/Taha_Hussein.jpg','username' => 'Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300),
+        );
+        return $this->render('author/showAuthor.html.twig',['id' => $id , 'authors' => $authors]);
     
+    }
 }
